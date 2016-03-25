6535c07f0717db2dc15dc3db0e1eb2134636d710
Jason Stirnaman <stirnamanj@gmail.com>
2016-03-25 16:55:38 -0500

    Display markdown editor for each publication in Entrez results.

--- assets/js/search_pubmed.js
+++ assets/js/search_pubmed.js
@@ -14,40 +14,48 @@ $(document).ready(function(){
       
     // Chain request for result IDs with request for citation records.
     // Get list of IDs based on search terms
-    $.getJSON('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json', { term : term, format: "json" })
+    $.getJSON(
+        'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json',
+        { term : term, format: "json" }
+      )
 
-    // Get results from a list of IDs 
-    // e.g. http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=11850928,11482001&format=json
+      // If successful, fetch summaries from the list of IDs 
+      // e.g. http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=11850928,11482001&format=json
      .then(function(results) {
         $("#status").text("Searching...")
         return $.getJSON('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=' + results.esearchresult.idlist.join() + '&retmode=json') 
       })
+      // Process summary results 
      .done(function(results) {
         $("#status").text("")
+        $("#citation-select").prepend('<h4 id="search-results">Search Results</h4>')
         if (results.result) {
-          $("#citation-select").prepend('<h4 id="search-results">Search Results</h4>')
           $.each(citations(results.result, publications).citationsMarkdown,
             function(i, cite) {
               var elem_id = i + '-' + cite.uid
-              var editor = $("#pico-editor .editor.form-group").clone()
-                           .attr('id', elem_id)
-              $(editor).appendTo("#pico-editor")
-              $('#'+elem_id).html(cite.markdown)
+              var editor = $("#pico-editor").clone()
+                .attr('id', elem_id)
+              $(editor).appendTo("#pico-studies")
+              $('#'+elem_id ".editor .editor-text").val(cite.markdown)
               $('#'+elem_id).show()
           })
+        }
+        else {
+          pub_errors.push("Search was successful, but no results were found")
         }
       })
       .fail(function() {
         $("#status").text("")
         pub_errors.push("Pubmed search failed. Please try again.")
-        for(var err in pub_errors) {
-          $("#flash").append( $("div.error", err) );
-        }
       })
       .always(function() {
         $("#submitbutton").prop( "disabled", false );
         $("#hints").show();
       });
+
+      for(var err in pub_errors) {
+        $("#flash").append( $("p.error", err) );
+      }
   }); 
 
   function repoLinks() {
